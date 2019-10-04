7a49938a6fa93fe302adbc5096e9aca50c2b5df9
StefoXP <stefano.pasquini.2010@gmail.com>
2019-10-04 13:23:57 +0200

    Aggiunti commenti SQL

--- sql/urbino_shop_query_Access.sql
+++ sql/urbino_shop_query_Access.sql
@@ -1,152 +1,152 @@
-spAddProdotto
+-- spAddProdotto
 INSERT INTO tblProdotto ( IDcategoria, Nome, Descrizione, Foto, Prezzo, Disponibilità )
 VALUES ([@categoria], [@nome], [@descrizione], [@foto], [@prezzo], [@disponibilita]);
 
-spAddUtente
+-- spAddUtente
 INSERT INTO tblUtente ( UserName, [Password], Nome, Indirizzo, Telefono, EMail, Ruolo )
 VALUES ([@UserName], [@Password], [@Nome], [@Indirizzo], [@Telefono], [@EMail], 2);
 
-spAggiornaCarrello
+-- spAggiornaCarrello
 UPDATE tblCarrello SET Quantità = [@quantita]
 WHERE IDutente=[@utente] AND IDprodotto=[@prodotto];
 
-spAggiornaCategoria
+-- spAggiornaCategoria
 UPDATE tblCategoria SET Nome = [@nome], Descrizione = [@descrizione], Foto = [@foto]
 WHERE IDcategoria=[@categoria];
 
-spAggiornaOrdine
+-- spAggiornaOrdine
 SELECT [IDutente], [IDprodotto], [Quantità]
 FROM tblOrdine;
 
-spAggiungiCarrello
+-- spAggiungiCarrello
 INSERT INTO tblCarrello ( IDutente, IDprodotto, Quantità )
 VALUES ([@IDutente], [@IDprodotto], [@Quantita]);
 
-spAggiungiCategoria
+-- spAggiungiCategoria
 INSERT INTO tblCategoria ( Nome, Descrizione, Foto )
 VALUES ([@nome], [@descrizione], [@foto]);
 
-spAggiungiOrdine
+-- spAggiungiOrdine
 SELECT *
 FROM tblOrdine
 WHERE IDutente=0;
 
-spEditProdotto
+-- spEditProdotto
 UPDATE tblProdotto SET IDcategoria = [@categoria], Nome = [@nome], Descrizione = [@descrizione], Foto = [@foto], Prezzo = [@prezzo], Disponibilità = [@disponibilita]
 WHERE IDprodotto=[@IDprodotto];
 
-spEditUtente
+-- spEditUtente
 UPDATE tblUtente SET UserName = [@UserName], [Password] = [@Password], Ruolo = [@Ruolo]
 WHERE IDutente=[@IDutente];
 
-spEliminaCarrello
+-- spEliminaCarrello
 DELETE *
 FROM tblCarrello
 WHERE IDutente=[@utente] AND IDprodotto=[@prodotto];
 
-spEliminaCategoria
+-- spEliminaCategoria
 DELETE [IDcategoria]
 FROM tblCategoria
 WHERE IDcategoria=[@categoria];
 
-spEliminaInteroCarrello
+-- spEliminaInteroCarrello
 DELETE *
 FROM tblCarrello
 WHERE IDutente=[@utente];
 
-spEliminaOrdine
+-- spEliminaOrdine
 DELETE *
 FROM tblOrdine
 WHERE IDutente=[@utente];
 
-spEliminaProdotto
+-- spEliminaProdotto
 DELETE *
 FROM tblProdotto
 WHERE IDprodotto=[@prodotto];
 
-spEliminaUtente
+-- spEliminaUtente
 DELETE *
 FROM tblUtente
 WHERE IDutente=[@utente];
 
-spLoginAdmin
+-- spLoginAdmin
 SELECT tblUtente.IDutente, tblRuolo.Ruolo
 FROM tblRuolo INNER JOIN tblUtente ON tblRuolo.IDruolo = tblUtente.Ruolo
 WHERE UserName=[@UserName] AND Password=[@Password];
 
-spLoginUtente
+-- spLoginUtente
 SELECT [IDutente]
 FROM tblUtente
 WHERE UserName=[@UserName] And Password=[@Password];
 
-spParzialiCarrello
+-- spParzialiCarrello
 SELECT [IDutente], [Quantità], [Prezzo], [Prezzo]*[Quantità] AS Parziale
 FROM tblProdotto INNER JOIN tblCarrello ON [tblProdotto].[IDprodotto]=[tblCarrello].[IDprodotto];
 
-spParzialiOrdine
+-- spParzialiOrdine
 SELECT [IDutente], [Quantità], [Prezzo], [Prezzo]*[Quantità] AS Parziale
 FROM tblProdotto INNER JOIN tblOrdine ON [tblProdotto].[IDprodotto]=[tblOrdine].[IDprodotto];
 
-spProdottoDettagli
+-- spProdottoDettagli
 SELECT *
 FROM tblProdotto
 WHERE IDprodotto=[@prodotto];
 
-spProdottoPerCategoria
+-- spProdottoPerCategoria
 SELECT *
 FROM tblProdotto
 WHERE IDcategoria=[@categoria];
 
-spProdottoRicerca
+-- spProdottoRicerca
 SELECT IDprodotto, tblProdotto.Nome AS Prodotto, tblCategoria.Nome AS Categoria, Prezzo, Disponibilità, tblProdotto.Foto
 FROM tblCategoria INNER JOIN tblProdotto ON tblCategoria.IDcategoria = tblProdotto.IDcategoria
 WHERE (((tblProdotto.Nome) Like [@prodotto]));
 
-spRuolo
+-- spRuolo
 SELECT *
 FROM tblRuolo
 ORDER BY Ruolo;
 
-spTotaliCarrello
+-- spTotaliCarrello
 SELECT IDutente, Sum(Quantità) AS TotaleQuantità, Sum(Parziale) AS TotaleSpesa
 FROM spParzialiCarrello
 GROUP BY IDutente
 HAVING IDutente=[@utente];
 
-spTotaliOrdine
+-- spTotaliOrdine
 SELECT IDutente, Sum(Quantità) AS TotaleQuantità, Sum(Parziale) AS TotaleSpesa
 FROM spParzialiOrdine
 GROUP BY IDutente
 HAVING IDutente=[@utente];
 
-spVerificaProdotto
+-- spVerificaProdotto
 SELECT Nome
 FROM tblProdotto
 WHERE Nome=[@nomeProdotto];
 
-spVerificaUtente
+-- spVerificaUtente
 SELECT UserName
 FROM tblUtente
 WHERE UserName=[@nomeUtente];
 
-spVisualizzaCarrello
+-- spVisualizzaCarrello
 SELECT [tblCarrello].[IDutente], [tblCarrello].[IDprodotto], [tblCarrello].[Quantità], [tblProdotto].[Nome], [tblProdotto].[Descrizione], [tblProdotto].[Foto], [tblProdotto].[Prezzo]
 FROM tblProdotto INNER JOIN tblCarrello ON [tblProdotto].[IDprodotto]=[tblCarrello].[IDprodotto]
 WHERE [tblCarrello].[IDutente]=[@utente]
 ORDER BY [tblProdotto].[Nome];
 
-spVisualizzaCategoria
+-- spVisualizzaCategoria
 SELECT *
 FROM tblCategoria
 ORDER BY Nome;
 
-spVisualizzaOrdine
+-- spVisualizzaOrdine
 SELECT tblOrdine.IDutente, tblProdotto.IDprodotto, tblOrdine.Quantità, tblProdotto.Nome, tblProdotto.Descrizione, tblProdotto.Foto, tblProdotto.Prezzo
 FROM tblProdotto INNER JOIN tblOrdine ON tblProdotto.IDprodotto = tblOrdine.IDprodotto
 WHERE (((tblOrdine.IDutente)=[@utente]))
 ORDER BY tblProdotto.Nome;
 
-spVisualizzaUtente
+-- spVisualizzaUtente
 SELECT *
 FROM tblUtente
 ORDER BY UserName;
